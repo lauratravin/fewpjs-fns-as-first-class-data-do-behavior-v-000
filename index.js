697d559889464d1c848aed01bd01ac2da6756264
Laura Messa Travin <laura.travin@gmail.com>
2021-04-17 20:14:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,6 +36,8 @@
 // }
 // ;;;;
 
+document.querySelector("button").addEventListener("click", handleClick);
+
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
