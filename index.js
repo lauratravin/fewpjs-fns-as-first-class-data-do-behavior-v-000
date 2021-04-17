83666f14e4135a9c7b534713581d45048774a8ef
Laura Messa Travin <laura.travin@gmail.com>
2021-04-17 20:12:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
-/* Given Code, don't edit */
-
-document.querySelector("button").addEventListener("click", handleClick);
+// /* Given Code, don't edit */
+// 
+// document.querySelector("button").addEventListener("click", handleClick);
+// 
+// function handleClick(e) {
+//   const timeString = document.getElementById('time').value
+//   displayMessage( greet(timeString) )
+// }
+// 
+// /* End Given Code, don't edit above here...*/
+// 
+// /* Write your implementation of greet() */
+// /* Write your implementation of displayMessage() */
+// function greet( myHour ){
+//   const theHour = myHour.split(":");
+//   if ( parseInt(theHour[0]) < 12 ) {
+//       return "Good Morning";
+//   } else if ( parseInt(theHour[0]) < 17 ) {
+//       return "Good Afternoon";
+//   } else {
+//       return "Good Evening";
+//  }
+// }
+// function displayMessage( myText){
+//   document.getElementById('greeting').innerText = myText;
+// 
+//   } else if ( parseInt(theHour[0]) < 17 ){
+//       return "Good Afternoon";
+//   } else {
+//       return "Good Evening";
+// }
+// 
+// function displayMessage( myText){
+//   document.getElementById('greeting').textContent = myText;
+// 
+// }
+;;;;
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
-  displayMessage( greet(timeString) )
+  displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
-function greet( myHour ){
-  const theHour = myHour.split(":");
-  if ( parseInt(theHour[0]) < 12 ) {
-      return "Good Morning";
-  } else if ( parseInt(theHour[0]) < 17 ) {
-      return "Good Afternoon";
-  } else {
-      return "Good Evening";
- }
-}
-function displayMessage( myText){
-  document.getElementById('greeting').innerText = myText;
-
-  } else if ( parseInt(theHour[0]) < 17 ){
-      return "Good Afternoon";
-  } else {
-      return "Good Evening";
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
 }
 
-function displayMessage( myText){
-  document.getElementById('greeting').textContent = myText;
-
-}
+/* Write your implementation of greet() */
+function greet(timeStr) {
+  /* It would be a good idea to make sure hour isn't less than 0 or greater
+   * than 24...perhaps "throwing an Error" if one of these impossible cases
+   * happens.
+   *
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Throwing_a_generic_error
+   *
+   */
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
