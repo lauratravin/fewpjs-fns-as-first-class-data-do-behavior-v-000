776c13be9e8b1d8df8a032a17a4a85f42898fd20
Laura Messa Travin <laura.travin@gmail.com>
2021-04-17 20:12:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // /* Given Code, don't edit */
-// 
+//
 // document.querySelector("button").addEventListener("click", handleClick);
-// 
+//
 // function handleClick(e) {
 //   const timeString = document.getElementById('time').value
 //   displayMessage( greet(timeString) )
 // }
-// 
+//
 // /* End Given Code, don't edit above here...*/
-// 
+//
 // /* Write your implementation of greet() */
 // /* Write your implementation of displayMessage() */
 // function greet( myHour ){
@@ -23,16 +23,16 @@
 // }
 // function displayMessage( myText){
 //   document.getElementById('greeting').innerText = myText;
-// 
+//
 //   } else if ( parseInt(theHour[0]) < 17 ){
 //       return "Good Afternoon";
 //   } else {
 //       return "Good Evening";
 // }
-// 
+//
 // function displayMessage( myText){
 //   document.getElementById('greeting').textContent = myText;
-// 
+//
 // }
 ;;;;
 
